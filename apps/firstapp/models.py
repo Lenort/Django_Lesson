823e26cfe77ed6fac9e83d5be24aeb1e2adbd691
django <Lenortteam@gmail.com>
2022-02-04 20:03:19 +0500

    Some comment

--- apps/firstapp/models.py
+++ apps/firstapp/models.py
@@ -1,3 +1,56 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Account(models.Model):
+    user = models.OneToOneField(
+        User,
+        on_delete=models.CASCADE
+    )
+    full_name = models.CharField(max_length=20)
+    description = models.TextField()
+    
+    def __str__(self)->str:
+        return f'Account:{self.user.id}/{self.full_name}'
+
+    class Meta:
+        ordering=(
+            'full_name',
+        )
+        verbose_name = 'Аккаунт'
+        verbose_name = 'Аккаунты'
+class Group(models.Model):
+    GROUP_NAME_MAX_LENGHT = 10
+
+    name = models.CharField(
+        max_length = GROUP_NAME_MAX_LENGHT
+    )
+    def _str__(self)->str:
+        return 'Группы (self.name)'
+    class Meta:
+        ordering = (
+            'name',
+        )
+        verbose_name = 'Группа'
+        verbose_name = 'Группы'
+class Student(models.Model):
+
+    accaunt = models.OneToOneField(
+        Account,
+        on_delete=models.CASCADE
+    )
+    age = models.IntegerField(
+        'Возраст студента'
+    )
+    gpa = models.FloatField(
+        'Средний балл'
+    )
+    group =models.ForeignKey(
+        Group,
+        on_delete=models.PROTECT
+    )
+    def __str__(self)->str:
+        return f'Возраст : {self.age}/n Группа {self.gpa}/n Оценка {self.group}'
+    class Meta:
+        ordering=('accaunt',)
+        verbose_name = 'Студент'
+        verbose_name = 'Студенты'
